60cc5ed4e4192ea4fefc2164e166d47d171257ee
JoshuaChett <joshuachett@gmail.com>
2024-03-26 09:12:26 -0400

    part 5

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,12 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "joshuachett+v1test@gmaail.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "AFT-OU"
+    SSOUserEmail              = "joshuachett+sso@gmail.com"
     SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    SSOUserLastName           = "Test"
   }
 
   account_tags = {
@@ -24,4 +24,33 @@ module "sandbox" {
   }
 
   account_customizations_name = "sandbox"
+}
+
+
+module "site1" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "joshuachett+v1site1@gmail.com"
+    AccountName               = "sandbox-aft"
+    ManagedOrganizationalUnit = "AFT-OU"
+    SSOUserEmail              = "joshuachett+sso@gmail.com"
+    SSOUserFirstName          = "AFT"
+    SSOUserLastName           = "Test"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "site-accounts"
 }
