cd8aa1a658315d8c6b76cff86d3dd65d00f7220d
JoshuaChett <joshuachett@gmail.com>
2024-03-26 17:00:45 -0400

    adding resource account

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,3 +54,31 @@ module "site1" {
 
   account_customizations_name = "site-accounts"
 }
+
+module "ResourceAccount" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "joshuachett+v1.Resource@gmail.com"
+    AccountName               = "ResourceAccount"
+    ManagedOrganizationalUnit = "Resources"
+    SSOUserEmail              = "joshuachett+sso@gmail.com"
+    SSOUserFirstName          = "Resource"
+    SSOUserLastName           = "acc"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = ""
+}
